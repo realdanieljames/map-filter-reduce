6b3f1aa4f716e1aafb9b375fbe79bb66d9b52d83
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-09-16 10:48:10 -0400

    map, reduce, exercise

--- main.js
+++ main.js
@@ -1,67 +1,119 @@
 // 1.  a. You are given an array of numbers below
-//     b. Using reduce subtract all of the numbers in the array from an initial number 200
-    let numbers = [12, 3, 5, 3, 5, 4]
+//     b. Using reduce 
+//      subtract all of the numbers in the array from an initial number 200
+let numbers = [12, 3, 5, 3, 5, 4]
 
-// 2.  Add 500 to each bottle of red wine, then add all the red wine bottles together. Use all three methods we learned today and use method chaining.
-    let data = [
+const subtractedFrom200 = numbers.reduce((accumulator) => {
+
+    return (200 - accumulator)
+}
+
+)
+
+//========================================================================================//
+//========================================================================================//
+// 2.  Add 500 to each bottle of red wine,
+//  then add all the red wine bottles together. 
+// Use all three methods we learned today and use method chaining.
+let data = [
     {
-    wineColor: 'red',
-    year: '1922',
-    region: 'Bordeaux',
-    numBottles: 123
+        wineColor: 'red',
+        year: '1922',
+        region: 'Bordeaux',
+        numBottles: 123
     },
     {
-    wineColor: 'red',
-    year: '1972',
-    region: 'Beaujolais',
-    numBottles: 29
+        wineColor: 'red',
+        year: '1972',
+        region: 'Beaujolais',
+        numBottles: 29
     },
     {
-    wineColor: 'white',
-    year: '2020',
-    region: 'Bourgogne',
-    numBottles: 224
+        wineColor: 'white',
+        year: '2020',
+        region: 'Bourgogne',
+        numBottles: 224
     },
     {
-    wineColor: 'white',
-    year: '1987',
-    region: 'Merlot',
-    numBottles: 122
+        wineColor: 'white',
+        year: '1987',
+        region: 'Merlot',
+        numBottles: 122
     },
     {
-    wineColor: 'white',
-    year: '1963',
-    region: 'Rioja',
-    numBottles: 22
+        wineColor: 'white',
+        year: '1963',
+        region: 'Rioja',
+        numBottles: 22
     },
     {
-    wineColor: 'red',
-    year: '1976',
-    region: 'Napa',
-    numBottles: 54
+        wineColor: 'red',
+        year: '1976',
+        region: 'Napa',
+        numBottles: 54
     }
-    ];
+];
 
+const redWineOnly = data.filter((dataArray) => {
+    if (dataArray.wineColor === 'red') {
+        return (dataArray)
+    }
+}
+)
+const allRedWineBottles = redWineOnly.map( (value, index, array)=>{
+    console.log(value.numBottles)
+} )
+
+console.log(redWineOnly)
+
+
+// add 500 to each bottle of redWine (wnieCOlor = red')
+
+//========================================================================================//
+//========================================================================================//
 // 3.  Just Averages
 //     - Using reduce, return an integer value for the average of all the index values and/or calculated values in an array.
 //     - If the Value is a String, use the character Code number for the first letter in the String.
 //     - Use all four arguments in the callback function in your solution
 //     - And a ternary wouldn't hurt
-      const nums1 = [8, 21.3, 16, 55, 22, 44]; // should be 27
-      const nums2 = [11.12, 43, 56.22, 78, 98, 11]; // should be 49
-      const nums3 = [2, 1222, 3444, 7254, 83.04444, 1111]; // should be 2186
-      const nums4 = [2, 1222,'sneeze', 3444, 7254, 8, 'abacus']; //should be 1734
+const nums1 = [8, 21.3, 16, 55, 22, 44]; // should be 27
+const nums2 = [11.12, 43, 56, 22, 78, 98, 11]; // should be 49
+const nums3 = [2, 1222, 3444, 7254, 83.04444, 1111]; // should be 2186
+const nums4 = [2, 1222, 'sneeze', 3444, 7254, 8, 'abacus']; //should be 1734
+
+//  function accepts arrays
+const indexAverager = nums1.reduce((total, currentValue, index) => {
+    console.log(index)
+
+})
+
+
+
+
+
+//========================================================================================//
+//========================================================================================//
+
 // 4.  Choose all the companies that started after 2000 and sort them ascending
-    let businesses = [
-    { company: 'Amex', startYear: 2000 },
+let businesses = [
+    { company: 'VISA', startYear: 2000 },
     { company: 'Chase', startYear: 1998 },
-    { company: 'Citibank', startYear: 2010 },
     { company: 'BOA', startYear: 1980 },
+    { company: 'Amex', startYear: 2000 },
     { company: 'TD', startYear: 2001 },
     { company: 'Mastercard', startYear: 2011 },
-    { company: 'VISA', startYear: 2000 }
-    ];
+    { company: 'Citibank', startYear: 2010 },
+];
 
+
+const year2000 = businesses.filter(business => business.startYear > 2000)
+
+const sortedYears = year2000.map( year => year.startYear).sort()
+
+console.log(sortedYears)
+
+//========================================================================================//
+//========================================================================================//
 // 5.  COMPUTE INTEGERS
 
 //     - use the array methods to multiply all integers in an array by 5.
@@ -69,6 +121,26 @@
 //       [1,2,undefined,6] should be [5,10,30]
 //       [1,3,25.5,4,32.9] should be [5,15,20]
 
+
+const numbers1 = [1, 2, 3, 4]
+const numbers2 = [1, 2, undefined, 6]
+const numbers3 = [1,3,25.5,4,32.9]
+
+const multiplyNumbersBy5 = numbers1.map((value) => {
+    if(typeof(value) === 'number'){
+
+    console.log(value * 5);
+}})
+
+
+
+
+
+
+
+
+//========================================================================================//
+//========================================================================================//
 // 6.
 
 // - Count the number times the same element value appears in an array and display your answer in an object with the element as the key and the number of times as the value
@@ -76,5 +148,21 @@
 // - The properties in the object will have a string for the key and a number for the value
 // - The key in each property should be the value in the array we are counting.
 // - The value in the object property should be the number of times a value appears in the original array.
-  const arr1 = [5,3,2,5,6]; //should be {'5':2,'3':1,'2':1,'6':1}
-  const arr2 = [3,1,2,5,2,5,7,5] // should be { '1': 1, '2': 2, '3': 1, '5': 3, '7': 1 }
+const arr1 = [5, 3, 2, 5, 6]; //should be {'5':2,'3':1,'2':1,'6':1}
+const arr2 = [3, 1, 2, 5, 2, 5, 7, 5] // should be { '1': 1, '2': 2, '3': 1, '5': 3, '7': 1 }
+
+const numberAppearance = arr1.filter((value, index, array) => {
+    let count = 0;
+    let result = {value: 'count'};
+
+    
+    console.log(value)
+    console.log(index)
+    console.log(array)
+    console.log(result)
+
+
+
+
+}
+)
